3e89f8d8d63957518a393730feb89dacd3f0279b
MD-AZMAL <azmalmd12@gmail.com>
2019-07-10 19:16:59 +0530

    show notification if new score available

--- whatscore.py
+++ whatscore.py
@@ -5,13 +5,17 @@ import subprocess as sp
 
 match_url = 'https://www.cricbuzz.com/cricket-match/live-scores'
 
-res = requests.get(match_url)
-soup = BeautifulSoup(res.text, 'html.parser')
+prevScore = ''
+while True:
+    res = requests.get(match_url)
+    soup = BeautifulSoup(res.text, 'html.parser')
 
-scoreDiv = soup.find('div',{'class': 'cb-lv-scrs-col'})
-additionalMessage = soup.find('div',{'class': 'cb-text-live'})
+    scoreDiv = soup.find('div',{'class': 'cb-lv-scrs-col'}).text
+    additionalMessage = soup.find('div',{'class': 'cb-text-live'}).text
 
-print(scoreDiv.text)
-print(additionalMessage.text)
+    if scoreDiv != prevScore:
 
-sp.run(['notify-send','{}\r{}'.format(scoreDiv.text,additionalMessage.text)])
+        print(scoreDiv)
+        print(additionalMessage)
+        sp.run(['notify-send','{}\r{}'.format(scoreDiv,additionalMessage)])
+    prevScore = scoreDiv
