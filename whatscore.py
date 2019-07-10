7a8983ec473b52bc058134eab02cb396b0b79e65
MD-AZMAL <azmalmd12@gmail.com>
2019-07-10 19:57:53 +0530

    Keyboard interupt

--- whatscore.py
+++ whatscore.py
@@ -5,17 +5,27 @@ import subprocess as sp
 
 match_url = 'https://www.cricbuzz.com/cricket-match/live-scores'
 
-prevScore = ''
-while True:
-    res = requests.get(match_url)
-    soup = BeautifulSoup(res.text, 'html.parser')
 
-    scoreDiv = soup.find('div',{'class': 'cb-lv-scrs-col'}).text
-    additionalMessage = soup.find('div',{'class': 'cb-text-live'}).text
 
-    if scoreDiv != prevScore:
+def getscore():
+    prevScore = ''
+    while True:
+        res = requests.get(match_url)
+        soup = BeautifulSoup(res.text, 'html.parser')
 
-        print(scoreDiv)
-        print(additionalMessage)
-        sp.run(['notify-send','{}\r{}'.format(scoreDiv,additionalMessage)])
-    prevScore = scoreDiv
+        scoreDiv = soup.find('div',{'class': 'cb-lv-scrs-col'}).text
+        additionalMessage = soup.find('div',{'class': 'cb-text-live'}).text
+
+        if scoreDiv != prevScore:
+
+            print(scoreDiv)
+            print(additionalMessage)
+            sp.run(['notify-send','{}\r{}'.format(scoreDiv,additionalMessage)])
+        prevScore = scoreDiv
+
+if __name__ == '__main__':
+    try:
+        getscore()
+    except KeyboardInterrupt:
+        print('Bye!')
+        exit(0)
