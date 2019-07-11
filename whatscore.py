bd9d718bce891e8fffce3af0ffc6c213c92c4e90
MD-AZMAL <azmalmd12@gmail.com>
2019-07-11 16:33:54 +0530

    feature update and bug fixes

--- whatscore.py
+++ whatscore.py
@@ -6,36 +6,46 @@ import time
 
 match_url = 'https://www.cricbuzz.com/cricket-match/live-scores'
 
-def notify_me(scoreDiv,additionalMessage):
+def notify_me(match,scoreDiv,additionalMessage):
+    print(match)
     print(scoreDiv)
     print(additionalMessage)
-    sp.run(['notify-send','{}\r{}'.format(scoreDiv,additionalMessage)])
+    print()
+    sp.run(['notify-send','{}\r{}\r{}'.format(match,scoreDiv,additionalMessage)])
 
 def getscore():
+
     startTime = time.time()
     prevScore = ''
+
     while True:
         res = requests.get(match_url)
         soup = BeautifulSoup(res.text, 'html.parser')
 
-        scoreDiv = soup.find('div',{'class': 'cb-lv-scrs-col'}).text 
+
+        scoreDiv = soup.find_all('div',{'class': 'cb-lv-scrs-col'})[0].text 
+        match = soup.find_all('a',{'class':'text-hvr-underline'})[0].text[:-1]
+
+        if scoreDiv[-1] != ')':
+            scoreDiv = scoreDiv + 'yet to bat'
         
         try:
-            additionalMessage =  soup.find('div',{'class': 'cb-text-complete'}).text
+            additionalMessage = soup.find_all('div',{'class': 'cb-text-live'})[0].text
         except:
             try:
-                additionalMessage = soup.find('div',{'class': 'cb-text-live'}).text
+                additionalMessage =  soup.find_all('div',{'class': 'cb-text-complete'})[0].text
             except:
                 additionalMessage = ''
-
+            
         if scoreDiv != prevScore:
-            notify_me(scoreDiv,additionalMessage)
+            notify_me(match,scoreDiv,additionalMessage)
         elif abs(int(time.time() - startTime)) >= 120:
             print(time.time() - startTime)
             startTime = time.time()
-            notify_me(scoreDiv,additionalMessage)
+            notify_me(match,scoreDiv,additionalMessage)
 
         prevScore = scoreDiv
+
 
 if __name__ == '__main__':
     try:
